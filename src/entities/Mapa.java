7927b9124060e08bc772cf3fb617feed0688eff4
wguardia94 <49293046+wguardia94@users.noreply.github.com>
2019-09-28 00:07:25 -0300

    Se agrego cargar tablero a partir de archivo

--- src/entities/Mapa.java
+++ src/entities/Mapa.java
@@ -11,17 +11,28 @@ import entities.Jugador;
 import entities.Minijuego;
 import entities.Personaje;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Mapa {
 
-	private Casilla[] tablero; // Por ahora un vector simple para testear
+	private Casilla[][] tablero;
 	private List<Personaje> jugadores = new LinkedList<Personaje>();
 	private List<Integer> turnos = new LinkedList<Integer>();
 	private Dado dado;
 	private int turnosPendientes;
 	private List<Minijuego> minijuegos = new ArrayList<Minijuego>();
-
-	// Constructor , aca comienza la partida
-	public Mapa(int cantCasillas, List<Jugador> listaJug) {
+	public void cargarTablero(Casilla[][] tablero) {
+		this.tablero = tablero;
+	}
+	
+	
+	public Mapa( List<Jugador> listaJug) throws FileNotFoundException {
 		int i = 0;
 		this.dado = new Dado(1, 6);
 		for (Jugador jug : listaJug) {
@@ -29,54 +40,73 @@ public class Mapa {
 			i++;
 		}
 		this.turnosPendientes = this.jugadores.size();
-		rellenarCasillas(cantCasillas);
+		rellenarCasillas("tablero.in");
 		ordenTurnos();
 		inicioJuego();
 	}
 	
-	// Alguna de las dos va a tener que volar de aca
-	public void cargarTablero(entities.Casilla[] tablero) {
-		this.tablero = tablero;
-	}
+	public void rellenarCasillas(String tArchivo) throws FileNotFoundException {
 
-	// Cargar casillas con efectos aleatorios
-	public void rellenarCasillas(int cant) {
-		this.tablero = new Casilla[cant+1];
-		for (int i = 0; i < this.tablero.length-1; i++) {
-			this.tablero[i] = new Casilla(i, this.dado.tirarDado());
+		Scanner sc = new Scanner(new File(tArchivo));
+		int fila;
+		int columna;
+		int j = 0;
+		String linea = sc.nextLine();
+		int puntRecor;
+		char caracter;
+
+		fila = Integer.parseInt(linea.split(" ")[0]);
+		columna = Integer.parseInt(linea.split(" ")[1]);
+		this.tablero = new Casilla[fila][columna];
+
+		for (int i = 0; i < fila; i++) {
+			linea = sc.nextLine();
+			char[] miArray = linea.toCharArray();
+			puntRecor = 0;
+			j=0;
+			while (j < columna) {
+				caracter = miArray[puntRecor];
+
+				switch (caracter) {
+				case '1':// casilla vacia
+					tablero[i][j] = new Casilla(i, j, 1, null);
+					break;
+				case '2':// casilla sumamonedas
+					tablero[i][j] = new Casilla(i, j, 2, "" + miArray[puntRecor + 1]);
+					break;
+				case '5':// casilla pierdemonedas
+					tablero[i][j] = new Casilla(i, j, 5, "" + miArray[puntRecor + 1]);
+					break;
+				case '7':// casilla union
+					tablero[i][j] = new Casilla(i, j, 7, "" + miArray[puntRecor + 1] + miArray[puntRecor + 2]);
+					break;
+				}
+
+				if (tablero[i][j].getTipoCasilla() == 1)
+					puntRecor++;
+				else if (tablero[i][j].getTipoCasilla() != 7)
+					puntRecor += 2;
+				else
+					puntRecor += 3;
+
+				j++;
+			}
+
 		}
-		this.tablero[this.tablero.length] = new Casilla(this.tablero.length, 7);
-	}
+		sc.close();
 
-	// Aca se ve el orden de cada uno
+	}
+	
 	public void ordenTurnos() {
 		for (Personaje pj : this.jugadores) {
 			this.turnos.add(pj.getNumJug());
 		}
 	}
-
+	
 	public void inicioJuego() {
 		// Aca se van a jugar las rondas hasta ver quien gana
 	}
-
-	public void siguienteTurno() {
-
-	}
-
-	public boolean esFinRonda() {
-		return this.turnosPendientes == 0;
-	}
-
-	public void finRonda() {
-		if (this.esFinRonda()) {
-			// Aca se jugara el minijuego entre los personajes , las recompensas y perdidas
-			// se veran
-			// segun el minijuego jugado
-		}
-	}
-
-	public static void main(String[] args) {
-
-	}
+	
 	
 }
+

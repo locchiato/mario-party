6046c5975969d9b1936255815d11af8aae6bb301
wguardia94 <49293046+wguardia94@users.noreply.github.com>
2019-11-06 14:18:16 -0300

    Se cambio if por switch en lo que lee el hilo del cliente que lee lo que el cliente envia

--- servidor/HiloDeCliente.java
+++ servidor/HiloDeCliente.java
@@ -47,84 +47,15 @@ public class HiloDeCliente extends Thread{
 				//RECIBIMOS EL OBJETO
 				Object peticion = in.readObject();
 				System.out.println(" Se recibio un objeto " + peticion.getClass().getName());
+				///Pregunta por el tipo de objeto recibido, por cada nueva clase para comunicacion
+				//se agrega un case
+				switch(peticion.getClass().getSimpleName()) {
 				
-				// REEMPLAZAR POR UN SWITCH
-				/*if( peticion instanceof IngresarPartida ){
-					//actualizarEspera((IngresarPartida)peticion);
-					buscarYagregar( (IngresarPartida)peticion );
-					//partida.agregarJugador( new Jugador(clientSocket,out,in));
-					frame.mostrarMensajeFrame("SE AGREGO EL JUGADOR " + clientSocket.getInetAddress());
-					//this.finalize();
-					// PERO SERIA UNA FORMA DE DETENER ESTE HILO PARA QUE LO
-					
-				}else if( peticion instanceof LoginBean ){
-					if(loguear((LoginBean) peticion)){
-						frame.mostrarMensajeFrame(ipCliente+">> Login");
-						((LoginBean)peticion).setIdUsuario(idUsuario);
-						out.writeObject(peticion);
-					}else
-						out.writeObject("ERROR LOGIN");
-				}else if( peticion instanceof Peticion ){
-					frame.mostrarMensajeFrame(ipCliente+">> Peticion");
-					out.writeObject("INGRESADO");
-				}else if( peticion instanceof RegistrarBean ){
-					if(agregarUsuario((RegistrarBean) peticion)){
-						frame.mostrarMensajeFrame(ipCliente+">> Registro");
-						out.writeObject("REGISTRO");
-					}else
-						out.writeObject("ERROR REGISTRO");
-				}else if( peticion instanceof DesconexionBean ){
-					desconectarUsuario();
-					frame.mostrarMensajeFrame(ipCliente+">> Logout");
-					out.writeObject("DESCONECTADO");
-				}else if( peticion.equals("KILL THREAD") ){
-					frame.mostrarMensajeFrame(ipCliente+">> Eliminando hilo de cliente");					
-					out.writeObject("HILO ELIMINADO");
-					break;//eliminamos bucle infinito
-				}else if( peticion instanceof RecuperarBean ){
-					frame.mostrarMensajeFrame(ipCliente+">> Solicitud de pregunta secreta.");
-					respuesta = devolverPreguntaSecreta((RecuperarBean) peticion);
-					if(respuesta == null)
-						out.writeObject("NICK INVALIDO");
-					else
-						out.writeObject(respuesta);
-				}else if( peticion instanceof DatosUsuarioBean ){
-					frame.mostrarMensajeFrame(ipCliente+">> Solicitud de datos del cliente.");
-					devolverDatosCliente((DatosUsuarioBean) peticion);
-					out.writeObject(peticion);
-				}else if( peticion instanceof ValidarRespuestaBean ){
-					frame.mostrarMensajeFrame(ipCliente+">> Solicitud de validar respuesta");					
-					respuesta = validarRespuesta((ValidarRespuestaBean) peticion); 
-					if(respuesta == null)
-						out.writeObject("RESPUESTA INVALIDA");
-					else
-						out.writeObject(respuesta);
-				}else if( peticion instanceof ExisteUsuarioBean ){
-					frame.mostrarMensajeFrame(ipCliente+">> Solicitud de existencia de usuario.");					
-					respuesta = existeUsuario((ExisteUsuarioBean) peticion); 
-					out.writeObject(respuesta);
-				}else if( peticion instanceof ActualizarDatosBean ){
-					frame.mostrarMensajeFrame(ipCliente+">> Solicitud de actualizacion de datos.");					
-					actualizarDatos((ActualizarDatosBean) peticion); 
-					out.writeObject("DATOS ACTUALIZADOS");
-				}else if( peticion instanceof estoyListoBean ){
-					System.out.println(((estoyListoBean) peticion).getId()-1);
-					partidas.get(((estoyListoBean)peticion).getIdPartida()).getJugadores().get( ((estoyListoBean) peticion).getId()-1).setEstoyListo(true);
-				}else if( peticion instanceof DireccionBean ){
-						partidas.get(((DireccionBean)peticion).getIdPartida()).getJugadores().get( ((DireccionBean) peticion).getId()-1).setDireccion(((DireccionBean) peticion).getDireccion());
-						partidas.get(((DireccionBean)peticion).getIdPartida()).getJugadores().get( ((DireccionBean) peticion).getId()-1).setEnvieDireccion(true);
-				}else if( peticion instanceof RankingBean ){
-					frame.mostrarMensajeFrame(ipCliente+">> Solicitud de ranking.");
-					out.writeObject(retornarRanking((RankingBean)peticion));
-				}else if( peticion instanceof EstadisticasBean ){
-					frame.mostrarMensajeFrame(ipCliente+">> Solicitud de estadisticas.");
-					out.writeObject(retornarEstadisticas((EstadisticasBean)peticion));
-				}else if( peticion instanceof ActualizarPartidasBean ){
-						enviarPartidas((ActualizarPartidasBean)peticion);
-						System.out.println("Se envio la actualizacion");
-				}else{
-					frame.mostrarMensajeFrame("no se reconoce al objeto.");
-				}*/
+				
+				
+				}
+				
+				
 			}
 		} catch (Throwable e) {
 			e.printStackTrace();
